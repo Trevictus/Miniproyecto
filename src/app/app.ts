58ad211e641eb09b3fe7b318e272732ec5e8d155
Víctor <vgomtej353@g.educaand.es>
2026-02-09 00:28:11 +0100

    Integrate Bootstrap into the project
    Adds Bootstrap 5.3.8 for styling, updating `package.json` and `angular.json`. Adjusts global styles and imports Bootstrap CSS.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {TaskListComponent} from './components/task-list/task-list';
-import {TaskFormComponent} from './components/task-form/task-form';
+import { TaskListComponent } from './components/task-list/task-list'; // El nombre de la clase es TaskListComponent [cite: 39, 52]
+import { TaskFormComponent } from './components/task-form/task-form'; // El nombre de la clase es TaskFormComponent [cite: 39, 44]
 
 @Component({
   selector: 'app-root',
+  standalone: true, // Asegúrate de que sea standalone si no usas módulos [cite: 43, 49]
   imports: [RouterOutlet, TaskListComponent, TaskFormComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
